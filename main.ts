ef7a89d8f441cc96e48a851736cf15914f50a583
TT-STEM <82073963+TT-STEM@users.noreply.github.com>
2022-09-21 14:39:12 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -80,22 +80,22 @@ namespace HC_controller {
             p2 = pins.analogReadPin(AnalogPin.P2)
             
             
-            if (p2 > 683) {
-                if (p1 > 683 && JOY_STATE != 3) {
+            if (p2 > 717) {
+                if (p1 > 717 && JOY_STATE != 3) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_D_DOWN
                     )
                     JOY_STATE = 3
                     control.waitMicros(wait_time)
-                } else if (p1 > 344 && p1 < 683 && JOY_STATE != 2) {
+                } else if (p1 >= 298 && p1 <= 717 && JOY_STATE != 2) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_1_DOWN
                     )
                     JOY_STATE = 2
                     control.waitMicros(wait_time)
-                } else if (p1 < 344 && JOY_STATE != 1) {
+                } else if (p1 < 298 && JOY_STATE != 1) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_A_DOWN
@@ -103,23 +103,22 @@ namespace HC_controller {
                     JOY_STATE = 1
                     control.waitMicros(wait_time)
                 }
-            } else if (p2 > 344) {
-                if (p1 > 683 && JOY_STATE != 6) {
+            } else if (p2 >= 298) {
+                if (p1 > 717 && JOY_STATE != 6) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_4_DOWN
                     )
                     JOY_STATE = 6
                     control.waitMicros(wait_time)
-                } else if (p1 > 344 && p1 < 683 && JOY_STATE != 5) {
+                } else if (p1 >= 298 && p1 <= 717 && JOY_STATE != 5) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_C_UP
                     )
                     JOY_STATE = 5
                     control.waitMicros(wait_time)
-                } else if (p1 < 344 && JOY_STATE != 4) {
-                    basic.showNumber(4)
+                } else if (p1 < 298 && JOY_STATE != 4) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_2_DOWN
@@ -128,21 +127,21 @@ namespace HC_controller {
                     control.waitMicros(wait_time)
                 }
             } else {
-                if (p1 > 683 && JOY_STATE != 9) {
+                if (p1 > 717 && JOY_STATE != 9) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_C_DOWN
                     )
                     JOY_STATE = 9
                     control.waitMicros(wait_time)
-                } else if (p1 > 344 && p1 < 683 && JOY_STATE != 8) {
+                } else if (p1 >= 298 && p1 <= 717 && JOY_STATE != 8) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_3_DOWN
                     )
                     JOY_STATE = 8
                     control.waitMicros(wait_time)
-                } else if (p1 < 344 && JOY_STATE != 7) {
+                } else if (p1 < 298 && JOY_STATE != 7) {
                     control.raiseEvent(
                     EventBusSource.MES_DPAD_CONTROLLER_ID,
                     EventBusValue.MES_DPAD_BUTTON_B_DOWN
